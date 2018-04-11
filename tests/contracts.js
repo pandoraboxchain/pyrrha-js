b9da93f0caa520970eff685c979338eba2fe04ab
Kostiantyn Smyrnov <kostysh@gmail.com>
2018-04-12 00:39:14 +0300

    ganache sandbox moved to separate repo

--- tests/contracts.js
+++ tests/contracts.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const path = require('path');
-const GanacheNode = require('./ganache');
+const GanacheNode = require('ganache-sandbox');
+const extract = ['Pandora', 'PandoraMarket'];
 
-module.exports = async (useServer = false) => {
+module.exports = async () => {
 
-    const node = new GanacheNode(path.join(__dirname, '../'), useServer);
+    const node = new GanacheNode({ path: path.join(__dirname, '../'), extract });
     const server = await node.server;
     const provider = await node.provider;
     const contracts = await node.contracts;
